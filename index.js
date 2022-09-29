dd7ee93d252e68c41e4fcbd1776d0031f1cdb820
Ivymarion <ivymarion2013@gmail.com>
2022-09-29 16:14:06 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(namesArray,event){
+    const newArray= [];
+    for (let i=0 ;i< namesArray.length ; i++){
+        newArray[i] = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`;
+        
+        console.log(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray;
+}
+
+// writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+
+function countDown(integer){
+    let i = 0;
+    while (i<=integer){
+        console.log(i);
+        i++;
+    }
+
+}
